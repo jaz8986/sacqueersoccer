ab638fcc00ddd0f6feb4f4767eade312b18cc642
Jaz Singh <jsingh8986@yahoo.com>
2023-07-11 23:00:24 -0700

    ready to deploy

---
+++ db/migrate/20230712054844_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :datetime
+      t.string :location
+      t.string :description
+      t.string :attends
+
+      t.timestamps
+    end
+  end
+end
