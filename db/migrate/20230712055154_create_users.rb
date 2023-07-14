a6f1016ed380f30ba05dc5b1fb7102f7864216cf
Jaz Singh <jsingh8986@yahoo.com>
2023-07-13 19:45:22 -0700

    installed bcrypt

--- db/migrate/20230712055154_create_users.rb
+++ db/migrate/20230712055154_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :instagram
       t.integer :phone
+      t.string :password_digest
       t.boolean :admin
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_055308) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "instagram"
     t.integer "phone"
+    t.string "password_digest"
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
